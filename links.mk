e1ae6c504859d73d72dbc5b440b5e7e6b2218083
Mehrnoosh <mehrnoosh.sadrzadeh@qmul.ac.uk>
2015-02-13 08:52:19 +0000

    test

---
+++ links.mk
@@ -0,0 +1,18 @@
+links:: lstlang0.sty
+lstlang0.sty: ~thor/tex/lstlang0.sty
+	ln -sf $< $@
+links:: lstlang1.sty
+lstlang1.sty: ~thor/tex/lstlang1.sty
+	ln -sf $< $@
+links:: bibdb.bib
+bibdb.bib: ~thor/tex/bibdb.bib
+	ln -sf $< $@
+links:: HySpirit.sty
+HySpirit.sty: ~thor/tex/HySpirit.sty
+	ln -sf $< $@
+links:: thormath.sty
+thormath.sty: ~thor/tex/thormath.sty
+	ln -sf $< $@
+links:: thorirmath.sty
+thorirmath.sty: ~thor/tex/thorirmath.sty
+	ln -sf $< $@
